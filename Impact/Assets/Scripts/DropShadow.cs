fa75329edda835744e92af3fc5e711d0f828bcaa
DantesGearbox <mads.engberg@hotmail.com>
2018-09-24 23:46:23 +0200

    Initial project

---
+++ Impact/Assets/Scripts/DropShadow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow : MonoBehaviour {
+
+	public Color shadowColor;
+	public float layerOrder = 0.0f;
+
+	private Vector2 offset = new Vector2(-0.8f, -0.4f);
+
+	private SpriteRenderer shadowCaster;
+	private SpriteRenderer shadow;
+
+	private Transform shadowTransform;
+	private Transform casterTransform;
+
+	// Use this for initialization
+	void Start () {
+
+		casterTransform = transform;
+		shadowTransform = new GameObject().transform;
+		shadowTransform.parent = casterTransform;
+		shadowTransform.gameObject.name = "DropShadow";
+		shadowTransform.localRotation = Quaternion.identity;
+		shadowTransform.localScale = Vector3.one;
+
+		shadowCaster = GetComponent<SpriteRenderer>();
+		shadow = shadowTransform.gameObject.AddComponent<SpriteRenderer>();
+		
+		shadow.sortingOrder = shadowCaster.sortingOrder - 1;
+		shadow.color = shadowColor;
+
+		shadowTransform.position = new Vector2(casterTransform.position.x + offset.x, casterTransform.position.y + offset.y);
+		shadow.sprite = shadowCaster.sprite;
+	}
+}
---
+++ Impact/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+		
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp (transform.position, new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z), 0.15f);
+	}
+}
