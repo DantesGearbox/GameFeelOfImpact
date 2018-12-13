9634698ffb887f7531e6097f17b99305096fd358
DantesGearbox <mads.engberg@hotmail.com>
2018-12-13 19:57:10 +0100

    Enemy behavior

---
+++ Impact/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	float timer = 25;
+	float time = 0;
+
+	private Rigidbody2D rb;
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rb.velocity = new Vector2(-3, rb.velocity.y);
+
+
+		time += Time.deltaTime;
+		if(time > timer) {
+			Destroy(this.gameObject);
+		}
+	}
+}
